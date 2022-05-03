1449924881f4a69a280b391cd2f1cbc12895377d
KhrystynaHirna <86721411+KhrystynaHirna@users.noreply.github.com>
2022-05-03 02:35:13 +0200

    transaction-history done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,11 @@
 import { Profile } from './social-profile/Profile';
 import { Statistics } from './statistics/Statistics';
 import { FriendList } from './friend-list/FriendList';
+import { TransactionHistory } from './transaction-history';
 import data from "./statistics/data.json";
 import user from "./social-profile/user.json";
 import friends from "./friend-list/friends.json";
-
+import transactions from "./transaction-history/transactions.json";
 import './App.css';
 
 
@@ -19,7 +20,8 @@ export const App = () => {
           stats={user.stats}
       />
       <Statistics title="Upload stats" data={data} />
-      <FriendList friends={ friends }/>
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
     </div>
   
   );
